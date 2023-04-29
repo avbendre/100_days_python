f7d7a2bfba8a2b9cf83d903bbe87707020c80ff2
Abhishek V Bendre <avbendre47@gmail.com>
2023-04-30 01:00:50 +0530

    bmi

---
+++ Day2/d2_2.py
@@ -0,0 +1,6 @@
+y=input("Input two digit numbers\n")
+print(y)
+x=str(y)
+#print(type(y))
+z=int(x[0])+int(x[1])
+print("Sum of two number is", z)
---
+++ Day2/d2_3.py
@@ -0,0 +1,6 @@
+#BMI calculator.
+w=int(input('Weight in kg'))
+h=int(input('height in cms'))
+d=(h/100)**2
+b=w/d
+print(b)
